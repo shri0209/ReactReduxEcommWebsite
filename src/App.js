a2814f0cc8fa65b7d1bbaa99c837818a77127eee
shri0209 <117466811+shri0209@users.noreply.github.com>
2023-02-20 12:38:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header/>
       <Routes>
-        <Route path='/' element={<Cards/>}/>
+        <Route path='/ReactReduxEcommWebsite' element={<Cards/>}/>
         <Route path='/cart/:id' element={<CardsDetails/>}/>
 
       </Routes>
